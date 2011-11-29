2a2e1f1eef54b9f02f29f82d312d4439409df349
Matt McCormick <mattps@ps30718.dreamhostps.com>
2011-11-29 12:19:39 -0800

    Initial commit.

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+    include_once('CAS.php');
+
+    phpCAS::setDebug();
+    phpCAS::client(CAS_VERSION_2_0, 'www.purdue.edu', 443, '/apps/account/cas');
+    // phpCAS::client(CAS_VERSION_2_0, 'webservices-test.itns.purdue.edu', 443, '/apps/account/cas-server-uber-webapp-3.4.6');
+
+    phpCAS::setNoCasServerValidation();
+
+    phpCAS::forceAuthentication();
+
+    if (isset($_REQUEST['logout']))
+    {
+        phpCAS::logout();
+    }
+
+?>
+<html>
+<head>
+    <title>Purdue CAS Example</title>
+</head>
+<body>
+    <h1>Authentication Succeeded</h1>
+    <p>
+        Hello, <b><?php echo phpCAS::getAttribute('fullname'); ?></b>!
+    </p>
+    <p>
+        Your username is <b><?php echo phpCAS::getUser(); ?></b>. Your PUID is <b><?php echo phpCAS::getAttribute('puid'); ?></b>. Your I2A2 characteristics are <b><?php echo phpCAS::getAttribute('i2a2characteristics'); ?></b>.
+    </p>
+    <p>
+        <a href="?logout=">Logout</a>
+    </p>
+</body>
+</html>
